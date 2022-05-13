aad11d6bf87bbe86fe6edeedab175cb5a6e04eb3
Shenghui Wu <793703860@qq.com>
2022-05-13 18:42:36 +0800

    util: fix the issue that memory usage not released after tracker Detach (#34592)
    ref pingcap/tidb#34571

--- util/memory/tracker.go
+++ util/memory/tracker.go
@@ -200,7 +200,12 @@ func reArrangeFallback(a ActionOnExceed, b ActionOnExceed) ActionOnExceed {
 
 // SetLabel sets the label of a Tracker.
 func (t *Tracker) SetLabel(label int) {
+	parent := t.getParent()
+	t.Detach()
 	t.label = label
+	if parent != nil {
+		t.AttachTo(parent)
+	}
 }
 
 // Label gets the label of a Tracker.
@@ -212,6 +217,10 @@ func (t *Tracker) Label() int {
 // already has a parent, this function will remove it from the old parent.
 // Its consumed memory usage is used to update all its ancestors.
 func (t *Tracker) AttachTo(parent *Tracker) {
+	if parent.isGlobal {
+		t.AttachToGlobalTracker(parent)
+		return
+	}
 	oldParent := t.getParent()
 	if oldParent != nil {
 		oldParent.remove(t)
