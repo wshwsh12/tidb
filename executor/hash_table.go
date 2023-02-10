81ca71580c21c23238ac422ae10bdbaca42626fd
wshwsh12 <793703860@qq.com>
2023-02-10 18:46:36 +0800

    remove some log

--- executor/hash_table.go
+++ executor/hash_table.go
@@ -275,15 +275,11 @@ func (c *hashRowContainer) GetMatchedRowsAndPtrs(probeKey uint64, probeRow chunk
 			memDelta += lastChunkSize
 		}
 		lastChunkBufPointer = c.chkBuf
-		if i%1000 == 0 {
-			logutil.BgLogger().Info("testJoinOOM", zap.Any("i", i))
-		}
 		if needTrackMemUsage && ((i & signalCheckpointForJoin) == (signalCheckpointForJoin - 1)) {
 			// Trigger Consume for checking the OOM Action signal
 			memDelta += int64(cap(matched))*rowSize + int64(cap(matchedPtrs))*rowPtrSize - matchedDataSize
 			matchedDataSize = int64(cap(matched))*rowSize + int64(cap(matchedPtrs))*rowPtrSize
 			c.memTracker.Consume(memDelta + 1)
-			logutil.BgLogger().Info("testJoinOOM, no panic", zap.Any("memory usage", c.sc.MemTracker.BytesConsumed()))
 			memDelta = 0
 		}
 		if !ok {
